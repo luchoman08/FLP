cb722d9f4bc27fdf9ad2cdee77f315792f81bc6f
luchoman08 <luis.manrique@correounivalle.edu.co>
2016-12-05 20:05:29 -0500

    for while globla terminados

---
+++ proyectoflp/README.py
@@ -0,0 +1,19 @@
+#Ejemplo 1
+
+var a = 3
+var b = 2
+var c = 3
+
+def funcion(x,y,z):
+	global s = 3
+	if evaluate x < 3 :
+		var t = 4 
+		evaluate evaluate t * x + evaluate y - z 
+	else:
+		var t = 2
+		evaluate evaluate t * y - z 
+	end 
+end
+
+print (execute funcion(a,b,c))
+print (s)
